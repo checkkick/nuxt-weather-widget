f800ef549681edf60b6719ac993efead77ac61ac
Konstantin <tuskancik226@gmail.com>
2024-02-20 12:40:11 +0300

    app config metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+
+  runtimeConfig: {
+    public: {
+      apiOpenWeather: '/api'
+    }
+  },
+
+  app: {
+    head: {
+      title: 'Nuxt Sono Weather Widget',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 })
