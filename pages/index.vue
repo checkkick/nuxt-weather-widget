60b8639b9f0e959c847629ce3b1c72a53858b3d3
Konstantin <tuskancik226@gmail.com>
2024-02-20 12:40:25 +0300

    create main page

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CurrentWeather />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CurrentWeather from '@/components/CurrentWeather.vue'
+</script>
+
+<style scoped></style>
