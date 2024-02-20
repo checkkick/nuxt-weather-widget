b231ac4ff5472dc3f31e3fcb85db77401884154c
Konstantin <tuskancik226@gmail.com>
2024-02-20 15:41:04 +0300

    create default styles

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
+  <div class="container">
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body,
+html {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif, serif;
+  background-color: #eceef2;
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  scroll-behavior: smooth;
+  box-sizing: inherit;
+}
+
+.container {
+  padding: 0 70px;
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
